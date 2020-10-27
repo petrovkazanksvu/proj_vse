442b5341f79b2b50495d7a73e808eb73510cfcce
petrovkazanksvu <petrovkazanksvu@mail.ru>
2020-10-27 15:07:14 +0000

    Conf ports

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
              #!/bin/bash
              echo "Hello, World" > index.html
-             nohup busybox httpd -f -p 8080 &
+             nohup busybox httpd -f -p ${var.server_port} &
              EOF
 
   tags = {
@@ -22,9 +22,15 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+var "server_port" {
+  description = "Port to server"
+  type        = number
+  default     = 8081
 }
